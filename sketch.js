51557c69f5bb68696b6a413b6ed998485da72782
RyanLopes7 <108070570+RyanLopes7@users.noreply.github.com>
2022-09-08 00:59:28 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -24,7 +24,7 @@ function setup() {
   world = engine.world;
 
   button = createImg();
-  button.position(400, 30);
+  button.position(250, 30);
   button.size(50, 50);
   button.mouseClicked(drop);
 
@@ -60,7 +60,7 @@ function draw() {
   right.show();
   left.show();
 
-  ellipse(fruit.position.x, fruit.position.y, 30, 30);
+  ellipse(fruit.position.x, fruit.position.y, 60, 30);
 
   Engine.update(engine);
 
